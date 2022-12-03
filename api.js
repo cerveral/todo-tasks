6d400c9709dbce15ee54950d42f679c942576cef
Alfredo Cervera <aca.1965@hotmail.com>
2022-12-03 13:40:45 -0500

    add .gitignore, add server, add package.json,  add api

---
+++ api.js
@@ -0,0 +1,21 @@
+var mongoose = require("mongoose");
+var express = require("express");
+var router = express.Router();
+var query = "mongodb+srv://alfredo:12345@cluster0.3naragb.mongodb.net/taskBD?retryWrites=true&w=majority"
+CD
+const db = (query);
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, function (error) {
+    if (error) {
+        console.log("Error!" + error);
+    } else {
+        console.log("Se ha conectado con la base de datos exitosamente");
+    }
+});
+
+module.exports = router;
