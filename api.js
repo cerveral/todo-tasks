61013c7e096c05497a1fb7db444467ff595f5b23
Alfredo Cervera <aca.1965@hotmail.com>
2022-12-04 16:27:54 -0500

    Variable de entorno

--- api.js
+++ api.js
@@ -2,7 +2,23 @@ var mongoose = require("mongoose");
 var express = require("express");
 var TaskModel = require('./task_schema');
 var router = express.Router();
-var query = "mongodb+srv://alfredo:12345@cluster0.3naragb.mongodb.net/taskBD?retryWrites=true&w=majority"
+
+let environment = null;
+
+if (!process.env.ON_HEROKU) {
+    console.log("Cargando variables de entorno desde archivo");
+    const env = require('node-env-file');
+    env(__dirname + '/.env');
+}
+
+environment = {
+    DBMONGOUSER: process.env.DBMONGOUSER,
+    DBMONGOPASS: process.env.DBMONGOPASS,
+    DBMONGOSERV: process.env.DBMONGOSERV,
+    DBMONGO: process.env.DBMONGO,
+};
+
+var query = 'mongodb+srv://' + environment.DBMONGOUSER + ':' + environment.DBMONGOPASS + '@' + environment.DBMONGOSERV + '/' + environment.DBMONGO + '?retryWrites=true&w=majority';
 
 const db = (query);
 
